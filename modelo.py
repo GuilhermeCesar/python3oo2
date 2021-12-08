6dbc729ef807fa112709690ffca92b1d2b51eae0
Guilherme Medeiros <guilherme.medeiros@platformbuilders.io>
2021-12-08 09:33:50 -0300

    Adicionar herança ao list

--- modelo.py
+++ modelo.py
@@ -40,13 +40,12 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} -  {self.ano} - {self.temporadas} temporadas -  {self._likes} Likes'
 
-class PlayList:
+class PlayList(list):
     def __init__(self, nome, programas):
         self.nome = nome
         self.programas = programas
+        super().__init__(programas)
 
-    def tamanho(self):
-        return len(self.programas)
 
 
 vingadores = Filme('Vingadores - Guerra infinita', 2018, 160)
@@ -65,8 +64,12 @@ atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+filmes_e_series = [vingadores, atlanta,  tmep]
 playlist_fim_de_semana = PlayList("Fim de semana", filmes_e_series)
 
-for programa in playlist_fim_de_semana.programas:
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
